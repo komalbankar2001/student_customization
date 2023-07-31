4c9795584be5a580753ce8221d5fb75c518c1b44
Komal <komal.bankar@pragtech.co.in>
2023-07-31 13:07:54 +0530

    [CHANGE] version

--- student_class/__manifest__.py
+++ student_class/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'Student Class',
-    'version' : '1.1',
+    'version' : '1.2',
     'summary': 'Student Class',
     'sequence': 10,
     'description': """Student Class""",
